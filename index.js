50e0e4ffee07ae6c6fcdc788b3a58c3cba935487
lenghuo01 <1946949433@qq.com>
2023-09-23 16:40:15 +0800

    koa

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 import Koa from'koa'
 import router from './src/router.js'
 import { valid } from './src/middleware/valid.js'
+import {koaBody} from 'koa-body'
 const server=new Koa()
+
 //验证用户信息
 server.use(valid)
+server.use(koaBody(
+    {
+        //启动
+        multipart:true,
+        formidable:{
+            uploadDir:`${process.cwd()}/upload`,//位置
+            maxFieldsSize:2*1024*1024,//2gb,
+            keepExtensions:true,//文件后缀
+            onFileBegin:(name,file)=>{
+                const fileName=`tuchuang_${file.newFilename}`//file.newfilename文件原名称
+                file.filepath=`${file.filepath.replace(file.newFilename,fileName)}`
+            }
+        }
+    }
+))
 //处理业务逻辑
 server.use(router.routes()).use(router.allowedMethods())
 
--- src/router.js
+++ src/router.js
@@ -5,4 +5,18 @@ router.get('/login',(ctx)=>{
     ctx.body='hello word222'
 })
 
+router.post('/:userId',(ctx)=>{
+    //响应体
+    ctx.body='hello word222'
+   //取头部 ctx.header
+   //取传入参数  ctx.query
+   //取路径参数 ctx.params
+   //取body  ctx.request.body
+   console.log(ctx.request.body)
+   //取body中文件 ctx.request.files.filename
+   ctx.req.on('data', (chunk) => {
+    console.log(chunk.toString())
+  })
+})
+
 export default router
